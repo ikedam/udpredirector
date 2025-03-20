34d6273f916c02af97eece33c2076ce8cb402fe1
ikedam <devld@ikedam.jp>
2025-03-20 14:30:37 +0900

    Output pid file

--- main.go
+++ main.go
@@ -1,96 +1,143 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"os"
 	"syscall"
 
 	"github.com/spf13/cobra"
 )
 
 func main() {
+	var pidFile string
+
 	var rootCmd = &cobra.Command{
 		Use:   "udpredirector [port] [mac] [interface]",
 		Short: "UDP Redirector forwards UDP packets to a specific MAC address",
 		Args:  cobra.ExactArgs(3), // 必須の位置引数を3つに設定
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			// 位置引数を取得
 			port := args[0]
 			targetMACAddr := args[1]
 			interfaceName := args[2]
 
-			// MACアドレスをパース
-			targetMAC, err := net.ParseMAC(targetMACAddr)
-			if err != nil {
-				log.Fatalf("Invalid MAC address: %v", err)
-			}
-
-			// UDPの受信ポートを指定
-			listenAddr := ":" + port
-			conn, err := net.ListenPacket("udp", listenAddr)
-			if err != nil {
-				log.Fatalf("Failed to listen on UDP port: %v", err)
-			}
-			defer conn.Close()
-
-			log.Printf("Listening for UDP packets on %s", listenAddr)
-
-			// ネットワークインターフェースを取得
-			iface, err := net.InterfaceByName(interfaceName)
-			if err != nil {
-				log.Fatalf("Failed to get interface %s: %v", interfaceName, err)
-			}
-
-			// RAWソケットを作成
-			fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(syscall.ETH_P_ALL))
-			if err != nil {
-				log.Fatalf("Failed to create raw socket: %v", err)
-			}
-			defer syscall.Close(fd)
-
-			// ソケットをインターフェースにバインド
-			addr := syscall.SockaddrLinklayer{
-				Protocol: syscall.ETH_P_ALL,
-				Ifindex:  iface.Index,
-			}
-			if err := syscall.Bind(fd, &addr); err != nil {
-				log.Fatalf("Failed to bind raw socket: %v", err)
-			}
-
-			buffer := make([]byte, 1500) // 最大パケットサイズ
-
-			for {
-				// UDPパケットを受信
-				n, addr, err := conn.ReadFrom(buffer)
-				if err != nil {
-					log.Printf("Error reading UDP packet: %v", err)
-					continue
-				}
-
-				log.Printf("Received %d bytes from %s", n, addr)
-
-				// Ethernetフレームを構築
-				ethFrame := append(targetMAC, iface.HardwareAddr...) // 宛先MAC + 送信元MAC
-				ethFrame = append(ethFrame, 0x08, 0x00)              // EtherType (IPv4)
-				ethFrame = append(ethFrame, buffer[:n]...)           // ペイロード
-
-				// RAWソケットで送信
-				err = syscall.Sendto(fd, ethFrame, 0, &syscall.SockaddrLinklayer{
-					Ifindex:  iface.Index,
-					Protocol: syscall.ETH_P_ALL,
-				})
-				if err != nil {
-					log.Printf("Error sending Ethernet frame: %v", err)
-					continue
-				}
-
-				log.Printf("Forwarded packet to MAC %s", targetMAC)
-			}
+			return mainImpl(port, targetMACAddr, interfaceName, pidFile)
 		},
 	}
 
+	// フラグの追加
+	rootCmd.Flags().StringVarP(&pidFile, "pidfile", "p", "/var/run/udpredirector.pid", "Path to the PID file")
+
 	// コマンドの実行
 	if err := rootCmd.Execute(); err != nil {
-		log.Fatalf("Error: %v", err)
+		log.Fatalf("Error: %+v", err)
+	}
+}
+
+func mainImpl(port, targetMACAddr, interfaceName, pidFile string) error {
+	if pidFile != "" {
+		// PIDファイルを作成
+		if err := writePIDFile(pidFile); err != nil {
+			return err
+		}
+		defer removePIDFile(pidFile)
+	}
+
+	// MACアドレスをパース
+	targetMAC, err := net.ParseMAC(targetMACAddr)
+	if err != nil {
+		return fmt.Errorf("invalid MAC address: %w", err)
+	}
+
+	// UDPの受信ポートを指定
+	listenAddr := ":" + port
+	conn, err := net.ListenPacket("udp", listenAddr)
+	if err != nil {
+		return fmt.Errorf("failed to listen on UDP port: %w", err)
+	}
+	defer conn.Close()
+
+	log.Printf("Listening for UDP packets on %s", listenAddr)
+
+	// ネットワークインターフェースを取得
+	iface, err := net.InterfaceByName(interfaceName)
+	if err != nil {
+		return fmt.Errorf("failed to get interface %s: %w", interfaceName, err)
+	}
+
+	// RAWソケットを作成
+	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(syscall.ETH_P_ALL))
+	if err != nil {
+		return fmt.Errorf("failed to create raw socket: %w", err)
+	}
+	defer syscall.Close(fd)
+
+	// ソケットをインターフェースにバインド
+	addr := syscall.SockaddrLinklayer{
+		Protocol: syscall.ETH_P_ALL,
+		Ifindex:  iface.Index,
+	}
+	if err := syscall.Bind(fd, &addr); err != nil {
+		return fmt.Errorf("failed to bind raw socket: %w", err)
+	}
+
+	buffer := make([]byte, 1500) // 最大パケットサイズ
+
+	for {
+		// UDPパケットを受信
+		n, addr, err := conn.ReadFrom(buffer)
+		if err != nil {
+			log.Printf("Error reading UDP packet: %v", err)
+			continue
+		}
+
+		log.Printf("Received %d bytes from %s", n, addr)
+
+		// Ethernetフレームを構築
+		ethFrame := append(targetMAC, iface.HardwareAddr...) // 宛先MAC + 送信元MAC
+		ethFrame = append(ethFrame, 0x08, 0x00)              // EtherType (IPv4)
+		ethFrame = append(ethFrame, buffer[:n]...)           // ペイロード
+
+		// RAWソケットで送信
+		err = syscall.Sendto(fd, ethFrame, 0, &syscall.SockaddrLinklayer{
+			Ifindex:  iface.Index,
+			Protocol: syscall.ETH_P_ALL,
+		})
+		if err != nil {
+			log.Printf("Error sending Ethernet frame: %v", err)
+			continue
+		}
+
+		log.Printf("Forwarded packet to MAC %s", targetMAC)
+	}
+
+	// return nil
+}
+
+func writePIDFile(pidFile string) error {
+	pid := os.Getpid()
+	file, err := os.Create(pidFile)
+	if err != nil {
+		return fmt.Errorf("failed to create PID file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprintf(file, "%d", pid)
+	if err != nil {
+		return fmt.Errorf("failed to write to PID file: %w", err)
+	}
+
+	log.Printf("PID file created at %s with PID %d", pidFile, pid)
+	return nil
+}
+
+func removePIDFile(pidFile string) {
+	err := os.Remove(pidFile)
+	if err != nil {
+		log.Printf("Failed to remove PID file: %v", err)
+	} else {
+		log.Printf("PID file %s removed", pidFile)
 	}
 }
